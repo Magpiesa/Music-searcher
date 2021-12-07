d998021bab46f9bb70839ca5dd865eb1b9e6c035
小胡咩 <60345656+Sunny-HuCunsong@users.noreply.github.com>
2021-12-07 17:54:37 +0800

    Update music.php

--- core/music.php
+++ core/music.php
@@ -1,10 +1,10 @@
 <?php
 /**
  *
- * 音乐搜索器 - 函数声明
+ * 音乐搜索器 - 入口
  *
- * @author  MaiCong <i@maicong.me>
- * @link    https://github.com/maicong/music
+ * @author  小胡咩 <1642157054@qq.com>
+ * @link    https://github.com/Sunny-HuCunsong/Music-searcher
  * @since   1.6.2
  *
  */
