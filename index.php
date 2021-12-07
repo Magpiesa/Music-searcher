773060f76f5b249131a625cef4f85ee67661a120
小胡咩 <60345656+Sunny-HuCunsong@users.noreply.github.com>
2021-12-07 17:51:57 +0800

    Update index.php
    增加卷曲 模块判断

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
  *
  * 音乐搜索器 - 入口
  *
- * @author  MaiCong <i@maicong.me>
- * @link    https://github.com/maicong/music
+ * @author  小胡咩 <1642157054@qq.com>
+ * @link    https://github.com/Sunny-HuCunsong/Music-searcher
  * @since   1.6.2
  *
  */
